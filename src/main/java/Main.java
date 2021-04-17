1920d1559cdca3323e6b3953014d2706cdec0998
Michal Maniewski <mpmaniewski@gmail.com>
2021-04-17 12:31:43 +0200

    connection to database

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306";
+        String username = "sql11406222";
+        String password = "ktG35u5HAA";
+        System.out.println("Connecting to server...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Server connected!");
+            Statement stmt = null;
+            ResultSet resultset = null;
+
+            try {
+                stmt = connection.createStatement();
+                resultset = stmt.executeQuery("SHOW DATABASES;");
+
+                if (stmt.execute("SHOW DATABASES;")) {
+                    resultset = stmt.getResultSet();
+                }
+
+                while (resultset.next()) {
+                    System.out.println(resultset.getString("Database"));
+                }
+            } catch (SQLException ex) {
+                // handle any errors
+                ex.printStackTrace();
+            } finally {
+                // release resources
+                if (resultset != null) {
+                    try {
+                        resultset.close();
+                    } catch (SQLException sqlEx) {
+                    }
+                    resultset = null;
+                }
+
+                if (stmt != null) {
+                    try {
+                        stmt.close();
+                    } catch (SQLException sqlEx) {
+                    }
+                    stmt = null;
+                }
+
+                if (connection != null) {
+                    connection.close();
+                }
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the server!", e);
+        }
+    }
+
+}
+
